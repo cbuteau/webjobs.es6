677bbebc53a2a623fb1fc4553d0d291dd1de935c
Christopher Buteau <cbuteau@gmail.com>
2020-09-08 20:29:35 -0400

    placeholder

---
+++ jobs/BinaryProcessingJob.js
@@ -0,0 +1,25 @@
+
+// checked in ...KimCosmic song...
+// from here... https://freemusicarchive.org/search?adv=1&quicksearch=dub&&
+// yopu can pick something else.
+
+class BinaryProcessingJob {
+  constructor() {}
+
+  dispatch(workerId, parameters) {
+    return new Promise(function(resolve, reject) {
+      // we might need to pass in appPath so they can form a file filepath.
+      var filePath = parameters.filePath;
+      // load file
+      // Move to TypedArray.
+      // Calculate mean and something else
+
+      resolve({
+        array: new Uint8Array(200),
+        count: 200
+      });
+    });
+  }
+}
+
+export {BinaryProcessingJob as Job}
